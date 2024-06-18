e36ad02b744d206303b808c1688e1f2a80d11ebf
Aleх Bark <thrtth@yandex.ru>
2024-06-18 16:27:27 +0700

    Refactoring

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+import os
 
+from flask import Flask
+from flask.cli import load_dotenv
+
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route("/")
