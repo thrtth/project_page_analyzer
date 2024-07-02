2a433836ddce5f5cb199b617fe2a64bf6643778e
Aleх Bark <thrtth@yandex.ru>
2024-07-02 17:11:57 +0700

    Lint bug fix

--- page_analyzer/db_queries/queries.py
+++ page_analyzer/db_queries/queries.py
@@ -24,8 +24,8 @@ def insert_url(cur, url):
 
 def select_url(cur, url_id):
     cur.execute("""
-                SELECT * 
-                FROM urls 
+                SELECT *
+                FROM urls
                 WHERE id = %s;
                 """,
                 (url_id,))
@@ -35,15 +35,15 @@ def select_url(cur, url_id):
 def select_checks(cur, url_id):
     cur.execute("""
                 SELECT
-                url_checks.id, 
-                url_checks.status_code, 
-                url_checks.h1, 
-                url_checks.title, 
-                url_checks.description, 
-                url_checks.created_at 
-                FROM url_checks 
-                JOIN urls 
-                ON urls.id = url_id 
+                url_checks.id,
+                url_checks.status_code,
+                url_checks.h1,
+                url_checks.title,
+                url_checks.description,
+                url_checks.created_at
+                FROM url_checks
+                JOIN urls
+                ON urls.id = url_id
                 WHERE urls.id = %s;
                 """,
                 (url_id,))
@@ -52,15 +52,15 @@ def select_checks(cur, url_id):
 
 def select_last_checks(cur):
     cur.execute("""
-                SELECT 
-                urls.id, 
-                urls.name, 
-                MAX(url_checks.created_at) 
-                FROM urls 
-                LEFT JOIN url_checks 
-                ON urls.id = url_checks.url_id 
-                GROUP BY urls.id, urls.name 
-                ORDER BY urls.created_at 
+                SELECT
+                urls.id,
+                urls.name,
+                MAX(url_checks.created_at)
+                FROM urls
+                LEFT JOIN url_checks
+                ON urls.id = url_checks.url_id
+                GROUP BY urls.id, urls.name
+                ORDER BY urls.created_at
                 DESC;
                 """)
     return cur.fetchall()
@@ -68,9 +68,9 @@ def select_last_checks(cur):
 
 def select_status_code(cur, url):
     cur.execute("""
-                SELECT status_code 
-                FROM url_checks 
-                WHERE url_id = %s AND created_at = %s
+                SELECT status_code
+                FROM url_checks
+                WHERE url_id = %s AND created_at = %s;
                 """,
                 (url[0], url[2]))
     return cur.fetchone()
@@ -78,9 +78,9 @@ def select_status_code(cur, url):
 
 def select_name(cur, url_id):
     cur.execute("""
-                SELECT name 
-                FROM urls 
-                WHERE id = %s
+                SELECT name
+                FROM urls
+                WHERE id = %s;
                 """,
                 (url_id,))
     return cur.fetchone()[0]
@@ -88,8 +88,8 @@ def select_name(cur, url_id):
 
 def insert_check(cur, url_id, status_code, h1, title, meta):
     cur.execute("""
-                INSERT INTO url_checks 
-                (url_id, status_code, h1, title, description, created_at) 
+                INSERT INTO url_checks
+                (url_id, status_code, h1, title, description, created_at)
                 VALUES (%s, %s, %s, %s, %s, %s);
                 """,
                 (url_id, status_code, h1, title, meta, datetime.now()))
