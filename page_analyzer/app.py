019507c3ce7f117550d311d0ae4aca38702ccd19
Aleх Bark <thrtth@yandex.ru>
2024-07-04 01:15:55 +0700

    Bug fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -24,6 +24,8 @@ def get_response(url):
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://")
 
 engine = create_engine(DATABASE_URL)
 
