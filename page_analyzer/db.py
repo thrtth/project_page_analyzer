46f391c2beec632c2f541ba8e4f090268feb2a03
Aleх Bark <thrtth@yandex.ru>
2024-07-16 18:20:52 +0700

    Lint bug fix

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,14 +1,15 @@
 import os
 from contextlib import contextmanager
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 # if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-#     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://")
+#     DATABASE_URL = DATABASE_URL
+#     .replace("postgres://", "postgresql+psycopg2://")
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
@@ -20,7 +21,7 @@ def db_session():
     try:
         yield session
         session.commit()
-    except:
+    except exc.SQLAlchemyError:
         session.rollback()
         raise
     finally:
